f93fbdf8c8c66bea2cf7c4a33335fe71670664a2
utkarsh srivastava <utkarshsrivastava.1205@gmail.com>
2025-08-08 17:35:23 +0530

    third commit

--- src/components/navbar/hero/HeroPic.jsx
+++ src/components/navbar/hero/HeroPic.jsx
@@ -6,25 +6,24 @@ import { fadeIn } from '@/framerMotion/variants';
 const HeroPic = () => {
   return (
     <motion.div 
-    variants={fadeIn('left',0.8)}
-          initial='hidden'
-          whileInView='show'
-          viewport={{once:false, amount:0.9}}
-    className='relative h-[600px] w-[600px] flex items-center justify-center mx-auto'>
-      
-      {/* Animated Square Behind */}
-      <div className='absolute -z-10 flex items-center justify-center animate-none '>
-        <BiSquare className='h-[500px] w-[600px] text-cyan-500 blur-md animate-[spin_20s_linear_infinite]'/>
-      </div>
+  variants={fadeIn('left', 0.2)}
+  initial='hidden'
+  whileInView='show'
+  viewport={{ once: false, amount: 0.2 }}
+  className='relative h-[600px] w-full max-w-[600px] flex items-center justify-center mx-auto'
+>
+  {/* Animated Square Behind */}
+  <div className='absolute inset-0 -z-10 flex items-center justify-center'>
+    <BiSquare className='h-[500px] w-[500px] text-cyan-500 blur-md animate-[spin_20s_linear_infinite]' />
+  </div>
 
-      {/* Your Logo Image */}
-      <img
-        src="/1.png"
-        alt="GROW MEDIA"
-        className='max-h-[350px] w-auto '
-      />
-      
-    </motion.div>
+  {/* Your Logo Image */}
+  <img
+    src="/1.png"
+    alt="GROW MEDIA"
+    className='max-h-[350px] w-auto max-w-full'
+  />
+</motion.div>
   );
 };
 
